9ba7fff943e3d7d29e21784f4f02c9f689eb808b
June Henriksen <june.henriksen2@nav.no>
2023-03-28 14:06:27 +0200

    Add pdfgenclient

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -1,8 +1,12 @@
 package no.nav.syfo.util
 
+import net.logstash.logback.argument.StructuredArguments
+
 const val NAV_CALL_ID_HEADER = "Nav-Call-Id"
 const val NAV_PERSONIDENT_HEADER = "nav-personident"
 const val TEMA_HEADER = "Tema"
 const val ALLE_TEMA_HEADERVERDI = "GEN"
 
 fun bearerHeader(token: String) = "Bearer $token"
+
+fun callIdArgument(callId: String) = StructuredArguments.keyValue("callId", callId)!!
