1019c75d1494b58c4bdc446af1d37d04acd8714f
geir-waagboe <geir.arne.waagbo@nav.no>
2023-03-28 13:03:06 +0200

    Add DocumentComponentDTO

--- src/main/kotlin/no/nav/syfo/melding/api/MeldingTilBehandlerRequestDTO.kt
+++ src/main/kotlin/no/nav/syfo/melding/api/MeldingTilBehandlerRequestDTO.kt
@@ -8,6 +8,7 @@ import java.util.*
 data class MeldingTilBehandlerRequestDTO(
     val behandlerRef: UUID,
     val tekst: String,
+    val document: List<DocumentComponentDTO> = emptyList(),
 )
 
 fun MeldingTilBehandlerRequestDTO.toMeldingTilBehandler(personident: PersonIdent): MeldingTilBehandler {
@@ -22,6 +23,7 @@ fun MeldingTilBehandlerRequestDTO.toMeldingTilBehandler(personident: PersonIdent
         arbeidstakerPersonIdent = personident,
         behandlerRef = behandlerRef,
         tekst = tekst,
+        document = document,
         antallVedlegg = 0 // TODO: Denne må vel komme fra frontend / regnes ut på en eller annen måte?
     )
 }
