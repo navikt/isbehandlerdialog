36ab338c5bf2ae42e27be6e8fbbd8da879ba65ed
June Henriksen <june.henriksen2@nav.no>
2023-04-05 13:46:30 +0200

    Add DokarkivClient

---
+++ src/main/kotlin/no/nav/syfo/client/dokarkiv/DokarkivClient.kt
@@ -0,0 +1,68 @@
+package no.nav.syfo.client.dokarkiv
+
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import net.logstash.logback.argument.StructuredArguments
+import no.nav.syfo.client.azuread.AzureAdClient
+import no.nav.syfo.client.dokarkiv.domain.JournalpostRequest
+import no.nav.syfo.client.dokarkiv.domain.JournalpostResponse
+import no.nav.syfo.client.httpClientDefault
+import no.nav.syfo.util.bearerHeader
+import org.slf4j.LoggerFactory
+
+class DokarkivClient(
+    private val azureAdClient: AzureAdClient,
+    private val dokarkivClientId: String,
+    dokarkivBaseUrl: String,
+) {
+    private val journalpostUrl: String = "$dokarkivBaseUrl$JOURNALPOST_PATH"
+
+    private val httpClient = httpClientDefault()
+
+    suspend fun journalfor(
+        journalpostRequest: JournalpostRequest,
+    ): JournalpostResponse? {
+        val accessToken = azureAdClient.getSystemToken(dokarkivClientId)?.accessToken
+        accessToken?.let { token ->
+            return try {
+                val response: HttpResponse = httpClient.post(journalpostUrl) {
+                    parameter(JOURNALPOST_PARAM_STRING, JOURNALPOST_PARAM_VALUE)
+                    header(HttpHeaders.Authorization, bearerHeader(token))
+                    accept(ContentType.Application.Json)
+                    contentType(ContentType.Application.Json)
+                    setBody(journalpostRequest)
+                }
+                val journalpostResponse = response.body<JournalpostResponse>()
+                COUNT_CALL_DOKARKIV_JOURNALPOST_SUCCESS.increment()
+                journalpostResponse
+            } catch (e: ClientRequestException) {
+                handleUnexpectedResponseException(e.response, e.message)
+            } catch (e: ServerResponseException) {
+                handleUnexpectedResponseException(e.response, e.message)
+            }
+        } ?: throw RuntimeException("Failed to Journalfor Journalpost: No accessToken was found")
+    }
+
+    private fun handleUnexpectedResponseException(
+        response: HttpResponse,
+        message: String?,
+    ): JournalpostResponse? {
+        log.error(
+            "Error while requesting Dokarkiv to Journalpost PDF with {}, {}",
+            StructuredArguments.keyValue("statusCode", response.status.value.toString()),
+            StructuredArguments.keyValue("message", message),
+        )
+        COUNT_CALL_DOKARKIV_JOURNALPOST_FAIL.increment()
+        return null
+    }
+
+    companion object {
+        const val JOURNALPOST_PATH = "/rest/journalpostapi/v1/journalpost"
+        private const val JOURNALPOST_PARAM_STRING = "forsoekFerdigstill"
+        private const val JOURNALPOST_PARAM_VALUE = true
+        private val log = LoggerFactory.getLogger(DokarkivClient::class.java)
+    }
+}
---
+++ src/main/kotlin/no/nav/syfo/client/dokarkiv/DokarkivMetric.kt
@@ -0,0 +1,20 @@
+package no.nav.syfo.client.dokarkiv
+
+import io.micrometer.core.instrument.Counter
+import no.nav.syfo.application.metric.METRICS_NS
+import no.nav.syfo.application.metric.METRICS_REGISTRY
+
+const val CALL_DOKARKIV_BASE = "${METRICS_NS}_call_dokarkiv"
+
+const val CALL_DOKARKIV_JOURNALPOST_BASE = "${CALL_DOKARKIV_BASE}_journalpost"
+const val CALL_DOKARKIV_JOURNALPOST_SUCCESS = "${CALL_DOKARKIV_JOURNALPOST_BASE}_success_count"
+const val CALL_DOKARKIV_JOURNALPOST_FAIL = "${CALL_DOKARKIV_JOURNALPOST_BASE}_fail_count"
+
+val COUNT_CALL_DOKARKIV_JOURNALPOST_SUCCESS: Counter = Counter
+    .builder(CALL_DOKARKIV_JOURNALPOST_SUCCESS)
+    .description("Counts the number of successful calls to Dokarkiv - Journalpost")
+    .register(METRICS_REGISTRY)
+val COUNT_CALL_DOKARKIV_JOURNALPOST_FAIL: Counter = Counter
+    .builder(CALL_DOKARKIV_JOURNALPOST_FAIL)
+    .description("Counts the number of failed calls to Dokarkiv - Journalpost")
+    .register(METRICS_REGISTRY)
