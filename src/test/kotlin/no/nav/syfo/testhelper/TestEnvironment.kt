0f5a56591f84525047a0f7d40e2f8838c4dd4820
geir-waagboe <geir.arne.waagbo@nav.no>
2023-11-02 14:20:31 +0100

    IS-1738: Enable melding til NAV in prod

--- src/test/kotlin/no/nav/syfo/testhelper/TestEnvironment.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/TestEnvironment.kt
@@ -64,7 +64,6 @@ fun testEnvironment() = Environment(
     legeerklaringBucketName = "test_bucket",
     legeerklaringVedleggBucketName = "vedlegg_bucket",
     cronjobAvvistMeldingStatusIntervalDelayMinutes = 60L * 4,
-    storeMeldingTilNAV = true,
 )
 
 fun testAppState() = ApplicationState(
