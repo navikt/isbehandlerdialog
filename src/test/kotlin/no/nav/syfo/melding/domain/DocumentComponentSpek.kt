55c8ee3d385fe8cba6c603116d88d927a84b337a
Eirik Dahlen <dahleneirik@gmail.com>
2023-11-07 09:47:37 +0100

    IS-1771: Remove illegal char from melding before pdfgen

--- src/test/kotlin/no/nav/syfo/melding/domain/DocumentComponentSpek.kt
+++ src/test/kotlin/no/nav/syfo/melding/domain/DocumentComponentSpek.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.melding.domain
 
+import no.nav.syfo.client.pdfgen.sanitizeForPdfGen
 import org.amshove.kluent.shouldBeEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -35,6 +36,25 @@ class DocumentComponentSpek : Spek({
                 
                 
             """.trimIndent()
+        }
+    }
+    describe("sanitizeForPdfGen") {
+        it("removes illegal characters") {
+            val documentWithIllegalChar = listOf(
+                DocumentComponentDTO(
+                    type = DocumentComponentType.PARAGRAPH,
+                    title = "tittel",
+                    texts = listOf("text1\u0002dsa", "text2"),
+                )
+            )
+            val expectedDocument = listOf(
+                DocumentComponentDTO(
+                    type = DocumentComponentType.PARAGRAPH,
+                    title = "tittel",
+                    texts = listOf("text1dsa", "text2"),
+                )
+            )
+            documentWithIllegalChar.sanitizeForPdfGen() shouldBeEqualTo expectedDocument
         }
     }
 })
