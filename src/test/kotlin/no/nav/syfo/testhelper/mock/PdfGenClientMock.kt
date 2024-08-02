4d385d01c83c018150c8eb66131ec01255cecefd
Vetle Bu Solgård <vetle.bu.solgard@nav.no>
2024-08-02 08:30:27 +0200

    IS-2343: Migrate to using ispdfgen for pdf generation (#169)

--- src/test/kotlin/no/nav/syfo/testhelper/mock/PdfGenClientMock.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/mock/PdfGenClientMock.kt
@@ -2,28 +2,28 @@ package no.nav.syfo.testhelper.mock
 
 import io.ktor.client.engine.mock.*
 import io.ktor.client.request.*
-import no.nav.syfo.client.pdfgen.PdfGenClient
 import no.nav.syfo.testhelper.UserConstants
 
 fun MockRequestHandleScope.pdfGenClientMockResponse(request: HttpRequestData): HttpResponseData {
     val requestUrl = request.url.encodedPath
+    val apiBasePath = "/api/v1/genpdf/isbehandlerdialog"
     return when {
-        requestUrl.endsWith(PdfGenClient.Companion.FORESPORSEL_OM_PASIENT_TILLEGGSOPPLYSNINGER_PATH) -> {
+        requestUrl.endsWith("$apiBasePath/foresporselompasient-tilleggsopplysninger") -> {
             respond(content = UserConstants.PDF_FORESPORSEL_OM_PASIENT_TILLEGGSOPPLYSNINGER)
         }
-        requestUrl.endsWith(PdfGenClient.Companion.FORESPORSEL_OM_PASIENT_LEGEERKLARING_PATH) -> {
+        requestUrl.endsWith("$apiBasePath/foresporselompasient-legeerklaring") -> {
             respond(content = UserConstants.PDF_FORESPORSEL_OM_PASIENT_LEGEERKLARING)
         }
-        requestUrl.endsWith(PdfGenClient.Companion.FORESPORSEL_OM_PASIENT_PAMINNELSE_PATH) -> {
+        requestUrl.endsWith("$apiBasePath/foresporselompasient-paminnelse") -> {
             respond(content = UserConstants.PDF_FORESPORSEL_OM_PASIENT_PAMINNELSE)
         }
-        requestUrl.endsWith(PdfGenClient.Companion.LEGEERKLARING_PATH) -> {
+        requestUrl.endsWith("/api/v1/genpdf/pale-2/pale-2") -> {
             respond(content = UserConstants.PDF_LEGEERKLARING)
         }
-        requestUrl.endsWith(PdfGenClient.Companion.RETUR_LEGEERKLARING_PATH) -> {
+        requestUrl.endsWith("$apiBasePath/henvendelse-retur-legeerklaring") -> {
             respond(content = UserConstants.PDF_RETUR_LEGEERKLARING)
         }
-        requestUrl.endsWith(PdfGenClient.Companion.HENVENDELSE_MELDING_FRA_NAV_PATH) -> {
+        requestUrl.endsWith("$apiBasePath/henvendelse-meldingfranav") -> {
             respond(content = UserConstants.PDF_HENVENDELSE_MELDING_FRA_NAV)
         }
 
