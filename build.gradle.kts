96d739f796572776da3b3c9b3be52cb7159e4b5d
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-11-21 09:19:13 +0100

    Bump dependencies (#191)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,26 @@
 group = "no.nav.syfo"
 version = "0.0.1"
 
-val flywayVersion = "9.22.3"
-val googleCloudStorageVersion = "2.30.1"
-val hikariVersion = "5.0.1"
-val jacksonDataTypeVersion = "2.16.0"
-val kafkaVersion = "3.6.0"
+val flywayVersion = "10.21.0"
+val googleCloudStorageVersion = "2.45.0"
+val hikariVersion = "5.1.0"
+val jacksonDataTypeVersion = "2.17.2"
+val kafkaVersion = "3.7.0"
 val kluentVersion = "1.73"
-val ktorVersion = "2.3.8"
-val logbackVersion = "1.4.14"
+val ktorVersion = "2.3.12"
+val logbackVersion = "1.5.7"
 val logstashEncoderVersion = "7.4"
-val micrometerRegistryVersion = "1.12.0"
-val mockkVersion = "1.13.8"
+val micrometerRegistryVersion = "1.12.2"
+val mockkVersion = "1.13.9"
 val nimbusJoseJwtVersion = "9.37.2"
-val postgresVersion = "42.7.2"
+val postgresVersion = "42.7.4"
 val postgresEmbeddedVersion = "2.0.7"
 val spekVersion = "2.0.19"
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("com.gradleup.shadow") version "8.3.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
+    id("com.gradleup.shadow") version "8.3.2"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
 repositories {
@@ -51,7 +51,7 @@ dependencies {
     // Database
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
     testImplementation("io.zonky.test:embedded-postgres:$postgresEmbeddedVersion")
 
     // (De-)serialization
@@ -66,7 +66,7 @@ dependencies {
         implementation("org.apache.zookeeper:zookeeper") {
             because("org.apache.kafka:kafka_2.13:$kafkaVersion -> https://www.cve.org/CVERecord?id=CVE-2023-44981")
             version {
-                require("3.8.3")
+                require("3.9.3")
             }
         }
     }
@@ -98,6 +98,7 @@ tasks {
     }
 
     shadowJar {
+        mergeServiceFiles()
         archiveBaseName.set("app")
         archiveClassifier.set("")
         archiveVersion.set("")
