f10a915619211f06b35881f98a0e16421059018a
geir-waagboe <geir.arne.waagbo@nav.no>
2025-11-05 14:40:22 +0100

    IS-3530: Fix broken masking of fnr

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,9 @@ dependencies {
     // Kafka
     val excludeLog4j = fun ExternalModuleDependency.() {
         exclude(group = "log4j")
+        exclude(group = "org.apache.logging.log4j")
     }
+
     implementation("org.apache.kafka:kafka_2.13:$kafkaVersion", excludeLog4j)
     implementation("io.confluent:kafka-avro-serializer:$confluent", excludeLog4j)
     constraints {
