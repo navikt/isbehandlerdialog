c99feacb0e223c8f4a4e23cb4f9a084f94fe156f
Peter Rydberg <peter.rydberg@bekk.no>
2026-06-09 15:09:19 +0200

    TEST DELETE

--- src/main/resources/db/migration/V4_22__delete_feilsendt_melding.sql
+++ src/main/resources/db/migration/V4_22__delete_feilsendt_melding.sql
@@ -1,10 +1,9 @@
 UPDATE MELDING
 SET tekst = '[Teksten er fjernet]',
     document = CASE
-       WHEN innkommende THEN '[]'::jsonb
-       ELSE '[{"key": null, "type": "PARAGRAPH", "texts": ["[Teksten er fjernet]"], "title": null}]'::jsonb
-    END
+                   WHEN innkommende THEN '[]'::jsonb
+                   ELSE '[{"key": null, "type": "PARAGRAPH", "texts": ["[Teksten er fjernet]"], "title": null}]'::jsonb
+        END
 WHERE uuid IN (
-   '2d110f94-885d-4bf7-a8e8-ec7ead941208',
-   '1685134e-2d5f-4744-9bbe-e9c5163f989b'
-);
+    '3388549a-179e-4a97-a0f5-fc046f0419c7'
+    );
